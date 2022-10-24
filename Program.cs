634a8c1b9082671becfdbd222c273543d5fa9266
DenisKa <pochta_dk@mail.ru>
2022-10-24 19:54:49 +0300

    Add task10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/*
 
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindSecondDigit (int number)
+{
+    int digit2 = (number /10) % 10;
+    return digit2;
+}
+
+Console.WriteLine("Input number: ");
+int num =Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 & num < 1000)
+{
+    int result = FindSecondDigit (num);
+    Console.WriteLine($"Second digit of {num} is {result}");
+}
+else
+{
+    Console.WriteLine($"Non-three-digit number entered. Try again!");
+}
+
+*/
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
