2ff6df48ac6c296f0140b9e0a255e6ec6e0ea703
DenisKa <pochta_dk@mail.ru>
2022-10-24 21:53:21 +0300

    Add task13

--- Program.cs
+++ Program.cs
@@ -14,18 +14,40 @@ int num =Convert.ToInt32(Console.ReadLine());
 if (num > 99 & num < 1000)
 {
     int result = FindSecondDigit (num);
-    Console.WriteLine($"Second digit of {num} is {result}");
+    Console.WriteLine($"The second digit of {num} is {result}");
 }
 else
 {
     Console.WriteLine($"Non-three-digit number entered. Try again!");
 }
 
-*/
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+void FindThirdDigit (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine($"No third digit");
+    }
+    else
+    {
+        int number1 = number;
+        while (number > 1000)
+        {
+        number = number / 10;
+        }
+        int digit3 = (number % 10);
+        Console.WriteLine($"The third digit of {number1} is {digit3}");
+    }
+}
 
+Console.WriteLine("Input number: ");
+int num =Convert.ToInt32(Console.ReadLine());
+
+FindThirdDigit (num);
+
+*/
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
